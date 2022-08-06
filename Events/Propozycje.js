398ca58b8e5654e13fa33ee92e156992f2a721cc
KvbuS <n3gativ@outlook.com>
2022-08-06 12:55:29 +0200

    Update Propozycje.js

--- Events/Propozycje.js
+++ Events/Propozycje.js
@@ -34,9 +34,19 @@ client.on(`messageCreate`, async (message) => {
     .setColor(client.config.primary)
 
     webhook.send({
-        username: message.user.username,
+        username: message.member.displayName,
         avatarURL: message.member.displayAvatarURL(),
         embeds: [embed]
+    }).then((msg) => {
+        msg.react(`<:tak:1001127925657124904>`)
+        msg.react(`<:moze:1001127928349872190>`)
+        msg.react(`<:nie:1001127927188041888>`)
+
+        msg.threads.create({
+            name: 'Komentarze'
+        }).then((thread) => {
+            thread.members.add(`${message.member.id}`)
+        })
     })
 
     message.delete()
