e15e3697c64db8a29ca5e6ddaa3b12afca266b65
kaafinski <szymonpel6@gmail.com>
2022-08-07 17:32:03 +0200

    betterhelp

--- Commands/Informacje/help.js
+++ Commands/Informacje/help.js
@@ -1,66 +1,98 @@
-const {
-    MessageEmbed,
-    MessageActionRow,
-    MessageSelectMenu,
-} = require("discord.js");
+const Discord = require('discord.js')
+
 module.exports = {
     name: "help",
-    cooldowns: 7000,
-    description: "Komenda pomocy",
-    type: "CHAT_INPUT",
-    run: async (client, interaction) => {
-        let arrayOfDevCommands = [
-            "eval",
-            "gban",
-            "guban",
-        ]
-        let arrayOfModCommands = [
-            "ban",
-            "kick",
-            'clear',
-            "ankieta",
-        ]
-        let arrayOfConfCommands = [
-            "autorola",
-            "ticket",
-            "tickety",
-            "nowosci",
-            "backup",
-            "lobby"
-        ]
-        let arrayOfInfoCommands = [
-            "help",
-            "vars"
-        ]
+    description: "Wyświetla podstawową pomoc",
+    /**
+     * @param {Discord.Client} client
+     * @param {Discord.Interaction} interaction
+     */
+    run: async (client, interaction, args) => {
+        const helpEmoji = {
+            informacje: client.emoji.question,
+            konfiguracja: client.emoji.settings
+        }
+        const directories = [
+            ...new Set(client.slashCommands.map((cmd) => cmd.directory)),
+        ];
+        const formatString = (str) => {
+            return `${str[0].toUpperCase()}${str.slice(1).toLowerCase()}`;
+        };
 
+        const categories = directories.map((dir) => {
+            const getCommands = client.slashCommands
+                .filter((cmd) => cmd.directory === dir)
+                .map((cmd) => {
+                    return {
+                        name: cmd.name,
+                        description: cmd.description
+                };
+            });
 
+            return {
+                directory: formatString(dir),
+                commands: getCommands,
+            };
+        });
 
-      let embed = new MessageEmbed()
-      .setTitle(`Panel pomocy ${client.user.username}`)
-      .setColor(client.config.primary)
-      .setThumbnail(client.user.avatarURL())
-      .setDescription(`**${client.user.username}** posiada \`${arrayOfDevCommands.length + arrayOfInfoCommands.length + arrayOfModCommands.length + arrayOfConfCommands.length}\` komend\nPrefix na tym serwerze to \`/\` (Slash)`)
-      .addFields(
-        {
-            name: `Moderacyjne`,
-            value: `\`${arrayOfModCommands.join(`, `)}\``
-        },
-        {
-            name: `Konfiguracyjne`,
-            value: `\`${arrayOfConfCommands.join(`, `)}\``
-        },
-        {
-            name: `Bot`,
-            value: `\`${arrayOfInfoCommands.join(`, `)}\``
-        },
-        {
-            name: `Dev`,
-            value: `\`${arrayOfDevCommands.join(`, `)}\``
-        },
-      )
+      const embed = new Discord.MessageEmbed()
+         .setAuthor({
+            name: "Podstawowa Pomoc",
+            iconURL: client.user.avatarURL()
+         })
+         .setDescription(
+            `${client.emoji.stagerequesttospeak} Witaj ${interaction.member}, jestem nowym wielofunkcyjnym botem na discorda!\n${client.emoji.slashcmd} Jestem **${client.user.username}** i obsługuję slash komendy.`
+         )
+         .setColor(client.config.primary)
+         .setTimestamp();
 
-      interaction.reply({
-        embeds: [embed]
-      })
-    },
-};
+      const components = (state) => [
+         new Discord.MessageActionRow().addComponents(
+            new Discord.MessageSelectMenu()
+               .setCustomId("help-menu")
+               .setPlaceholder("Wybierz moduł!")
+               .setDisabled(state)
+               .addOptions([
+                  categories.map((cmd) => {
+                     return {
+                        label: `${cmd.directory}`,
+                        value: `${cmd.directory.toLowerCase()}`,
+                        emoji: `${helpEmoji[cmd.directory.toLowerCase()] || client.emoji.folder}`,
+                        description:
+                           `Kliknij po więcej informacji`,
+                     };
+                  }),
+               ])
+         ),
+      ];
+
+      const inMessage = await interaction.reply({
+         embeds: [embed],
+         components: components(false),
+      });
+
+      const filter = (interactio) => interactio.user.id === interaction.user.id;
+
+      const collector = interaction.channel.createMessageComponentCollector({
+         filter,
+         componentType: "SELECT_MENU",
+         time: 60000,
+      });
+
+      collector.on("collect", (interactio) => {
+         const [directory] = interactio.values;
+         const category = categories.find(
+            (x) => x.directory.toLowerCase() === directory
+         );
+
+         const embed2 = new Discord.MessageEmbed()
+            .setAuthor({ name: `${directory.charAt(0).toUpperCase()}${directory.slice(1).toLowerCase()}`, iconURL: client.user.avatarURL()})
+            .setDescription(
+               "" + category.commands.map((cmd) => `\`${cmd.name}\` ${cmd.description}`).join("\n ")
+            )
+            .setColor(client.config.primary);
+
+         interactio.update({ embeds: [embed2] });
+      });   
+    }
+}
