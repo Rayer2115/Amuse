501922920b0c37468ad8f7b3cc35302576af8786
KvbuS <n3gativ@outlook.com>
2022-08-09 15:07:00 +0200

    Dodano zmienne statusy :anguished:

--- Events/ClientReady.js
+++ Events/ClientReady.js
@@ -3,10 +3,30 @@ const logger = require('aminologer')
 const schedule = require('node-schedule');
 
 client.on("ready", async () => {
+    let guilds = await client.guilds.fetch()
+    let status = [
+        `${guilds.size} serwery`,
+        `${client.users.cache.size} użytkowników`,
+        `${client.channels.cache.filter(c => c.type === `GUILD_TEXT` || c.type === `GUILD_VOICE`).size} kanały`
+    ]
+
+    let number = 0
+
     client.user.setActivity({
-        name: `Amuse (0) | ${client.guilds.cache.size}`,
+        name: `Amuse (0) | ${status[number]}`,
         type: "WATCHING"
     })
+
+    setInterval(() => {
+        item = status[number]
+        client.user.setActivity({
+            name: `Amuse (0) | ${item}`,
+            type: "WATCHING"
+        })
+
+        number++
+        if(number === 3) number = 0
+    }, 5000)
     client.logger.log(`Zalogowano jako ${client.user.tag}`)
 
     const rule = new schedule.RecurrenceRule();
